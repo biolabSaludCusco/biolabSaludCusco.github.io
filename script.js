e6ce302c80d4cceba41755b7a99e75a879b86d44
Sebastian <parejaseb19@gmail.com>
2024-01-05 11:39:32 -0500

    comiteado

---
+++ script.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#botonParaCerrar");
+
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+});
+
+
+cerrar.addEventListener("click", function() {
+    nav.classList.remove("visible");
+});
+
